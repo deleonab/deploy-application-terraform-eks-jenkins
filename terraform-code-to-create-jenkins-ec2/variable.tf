ec1309dd9053e450347ae502617ff59d36e51b5e
Dele Onabowu <delesodade@hotmail.com>
2023-02-19 08:20:53 +0000

    declare variables for vpc, internet gateway,subnet

--- terraform-code-to-create-jenkins-ec2/variable.tf
+++ terraform-code-to-create-jenkins-ec2/variable.tf
@@ -4,23 +4,23 @@ variable "avail_zone" {
   default=""
 }
 
-variable "subnet_cidr_block " {
+variable "subnet_cidr_block" {
   description = "cidr block for the vpc"
   default="10.0.1.0/24"
 }
 
-variable "vpc_cidr_block " {
+variable "vpc_cidr_block" {
   description = "cidr block for the subnet"
   default="10.0.0.0/16"
 }
 
-variable " env_prefix" {
+variable "env_prefix" {
   description = "prefix to append to other variables"
   type = string
   default=""
 }
 
-variable " instance_type" {
+variable "instance_type" {
   description = "instance type for our jenkins server"
   type = string
   default="t2.micro"
